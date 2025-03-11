1639126337db55e03e63cf4399d5115d2903d217
Dev-Alase <devalase5@gmail.com>
2025-03-11 15:19:44 +0530

    Wait, I am trying

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Hello, myself Devendra Alase : PES1UG2CS178 ";
+  cout << "Hello, myself Devendra Alase : PES1UG2CS178" <<endl;
   return 0;
 }
