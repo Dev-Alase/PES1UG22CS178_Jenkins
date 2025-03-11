b24c04f50c4fcd471a7b11d02bab85beb1b7e45c
Dev-Alase <devalase5@gmail.com>
2025-03-11 15:11:50 +0530

    Changes made to hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Hello, myself Devendra Alase : PES1UG2CS178"
   return 0;
 }
